81bca3fe59ce49a4ff2f14f131de863772301a65
Matthew Bodily <mattcbodily@gmail.com>
2019-09-13 13:47:41 -0600

    added some styling to the counter

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 let count = 0;
-
 const counter = document.getElementById('counter')
 counter.innerText = count;
 
 function incrementCount(){
-    count += 1;
+    count++;
+    if(count < 0){
+        counter.style = 'color: red';
+    } else if(count > 0){
+        counter.style = 'color: green';
+    } else {
+        counter.style = 'color: black';
+    }
     counter.innerText = count;
 }
 
 function decrementCount(){
-    count -= 1;
+    count--;
+    if(count < 0){
+        counter.style = 'color: red';
+    } else if(count > 0){
+        counter.style = 'color: green';
+    } else {
+        counter.style = 'color: black';
+    }
     counter.innerText = count;
 }
 
 function resetCount(){
     count = 0;
     counter.innerText = count;
+    counter.style = 'color: black';
 }
